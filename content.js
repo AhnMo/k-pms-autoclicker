c9f63e34328b4e0fd144fae13daa68c941fd2914
Park Junghwan <ahnmo@pagez.kr>
2018-10-10 10:57:05 +0900

    Testing done

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
 chrome.webNavigation.onCompleted.addListener(function() {
-  chrome.tabs.executeScript({code: 'form1.submit();"'});
-}, {url: [{urlMatches : 'http://k-pms.kaist.ac.kr/CPopupRequest'}]});
+  chrome.tabs.executeScript({code: 'form1.submit();'});
+}, {
+  url: [
+    { urlEquals : 'http://k-pms.kaist.ac.kr/CPopupRequest' }
+  ]
+});
